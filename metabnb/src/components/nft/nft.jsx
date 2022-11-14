db4fc542d8881f3d4596445b7b55ff172677690a
chelsea-banke <chelseanewhorizon@gmail.com>
2022-11-14 19:31:39 +0100

    restyle

--- metabnb/src/components/nft/nft.jsx
+++ metabnb/src/components/nft/nft.jsx
@@ -1,7 +1,7 @@
 import './nft.css'
 function NFT(props){
     return(
-        <div className="p-2 border border-gray-600 w-fit rounded-lg mb-5 NFT">
+        <div className="p-2 border border-gray-600 w-fit mb-5 NFT">
             <img src={props.url} className="nft"/>
             <div className='flex flex-row justify-between p-1'>
                 <div>Deseart Kind</div>
@@ -9,7 +9,7 @@ function NFT(props){
             </div>
             <div className='flex flex-row justify-between p-1'>
                 <div>2345km away</div>
-                <div>available for 2weeks</div>
+                <div>available for 2weeks stay</div>
             </div>
             <img src='icons/rating.svg' className='p-1'/>
         </div>
--- metabnb/src/components/places/places.jsx
+++ metabnb/src/components/places/places.jsx
@@ -5,7 +5,7 @@ function Places(){
     return(
         <div className="Places">
             <div className="flex flex-row justify-evenly w-screen pl-10 pr-20 mt-10 max-[830px]:text-sm max-[750px]:block max-[750px]:p-1">
-                <div className="flex flex-row flex-wrap justify-evenly w-11/12 max-[750px]:hidden">
+                <div className="flex flex-row flex-wrap justify-evenly w-11/12 max-[750px]:text-sm types">
                     <div>Resturant</div>
                     <div>Cottage</div>
                     <div>Castle</div>
@@ -15,13 +15,13 @@ function Places(){
                     <div>Off-Grid</div>
                     <div>Farm</div>
                 </div>
-                <div className="flex flex-row justify-between border border-gray-400 p-2 rounded-md max-[750px]:w-full settings-btn">
+                <div className="flex flex-row justify-between border border-gray-400 p-2 rounded-md max-[750px]:w-full max-[750px]:mt-5 settings-btn">
                     <p>Location</p>
                     <img className="ml-5" src="icons/settings.svg"/>
                 </div>
             </div>
             <div className='flex flex-row flex-wrap justify-evenly p-10 max-[407px]:p-0 max-[407px]:pt-10'>
-                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(i => <NFT url={'nfts/nft'+i+'.png'}/>)}
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(i => <NFT url={'nfts/nft'+i+'.png'}/>)}
             </div>
         </div>
     )
